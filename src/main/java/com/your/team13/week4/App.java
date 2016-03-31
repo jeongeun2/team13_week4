bc39687736ec65d680f00842088cb6065ecb9be5
IamSSam <kongse92@google.com>
2016-03-31 16:24:55 +0900

    MAIN CLASS COMMIT

--- src/main/java/com/your/team13/week4/App.java
+++ src/main/java/com/your/team13/week4/App.java
@@ -1,13 +1,40 @@
 package com.your.team13.week4;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		String plan;
+		int calltime;
+		int line_number;
+
+		Calculate calculate = new Calculate();
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Plan : ");
+		plan = sc.next();
+
+		if (plan.equals("Gold")) {
+			Gold gold = new Gold();
+		} else if (plan.equals("Silver")) {
+			Silver silver = new Silver();
+		}
+
+		System.out.println("총사용시간 : ");
+		calltime = sc.nextInt();
+
+		System.out.println("사용회선 : ");
+		line_number = sc.nextInt();
+
+		sc.close();
+
+		System.out.println("금액 : " + calculate.total_rate());
+	}
 }
--- src/main/java/com/your/team13/week4/Customer.java
+++ src/main/java/com/your/team13/week4/Customer.java
@@ -3,38 +3,38 @@ package com.your.team13.week4;
 public class Customer {
 
 	// optional
-	private String name;
-	private String address;
-	private String period_of_payment;// �����غ���!!
+	// private String name;
+	// private String address;
+	// private String period_of_payment;// �����غ���!!
 	private int line_number; // ��ȭ�� ��
 	private int calltime; // ���ð�
 
 	protected Plan plan; // �����
 
 	public Customer(String name, String address, String period_of_payment, Plan plan, int line_number, int time) {
-		this.name = name;
-		this.address = address;
-		this.period_of_payment = period_of_payment;
+		// this.name = name;
+		// this.address = address;
+		// this.period_of_payment = period_of_payment;
 		this.plan = plan;
 		this.line_number = line_number;
 		this.calltime = time;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
+	// public String getName() {
+	// return name;
+	// }
+	//
+	// public void setName(String name) {
+	// this.name = name;
+	// }
+	//
+	// public String getAddress() {
+	// return address;
+	// }
+	//
+	// public void setAddress(String address) {
+	// this.address = address;
+	// }
 
 	public int getLine_number() {
 		return line_number;
@@ -50,10 +50,6 @@ public class Customer {
 
 	public void setTime(int time) {
 		this.calltime = time;
-	}
-
-	public String toString() {
-		return "Name : " + this.getName() + ", " + "Address : " + this.getAddress();
 	}
 
 }
